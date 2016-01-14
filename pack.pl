ee0a6ed5e11bf6f0850c571e59050f46674f41c0
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-01-14 23:25:08 +0100

    version 3.0

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(cplint).
 title('A suite of programs for reasoning with probabilistic logic programs').
-version('2.0').
+version('3.0').
 author('Fabrizio Riguzzi','fabrizio.riguzzi@unife.it').
 
