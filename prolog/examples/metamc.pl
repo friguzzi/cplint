7078fe268951397519babcf7db8ffe242116d721
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-02-03 08:59:32 +0100

    mc_prob and mc_sample in the body of clauses + example

---
+++ prolog/examples/metamc.pl
@@ -0,0 +1,36 @@
+/*
+Probabilities computation in the body of probabilistic clauses.
+*/
+:- use_module(library(mcintyre)).
+
+:- if(current_predicate(use_rendering/1)).
+:- use_rendering(c3).
+:- endif.
+
+:- mc.
+
+:- set_mc(k,20).
+
+:- begin_lpad.
+
+a:0.2:-
+  mc_sample(b,20,P),
+  P>0.2.
+
+b:0.5:-
+  c.
+
+c.
+
+:- end_lpad.
+
+/** <examples>
+
+?- mc_prob(a,Prob).
+% expected result 0.2
+
+?- mc_sample(a,10,Prob).
+% expected result 0.2
+
+
+*/
