cf598c17da2d3ae650843b4aa9797af994069eee
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-02-15 19:22:03 +0100

    update with swish

--- prolog/examples/rmc.pl
+++ prolog/examples/rmc.pl
@@ -1,5 +1,6 @@
 /*
-Model checker for fuzzy formulas in generalized probabilistic logic (GPL).
+Model checker for Recursive Markov Chains expressed in 
+generalized probabilistic logic (GPL).
 GPL is an expressive logic based on the modal mu-calculus for probabilistic 
 systems.
 GPL is designed for model checking reactive probabilistic transition systems 
@@ -11,9 +12,11 @@ by Fabrizio Riguzzi.
 */
 
 /** <examples>
-?- mc_sample(models(s1(3), form(x(2)), []),10,P).
+?- mc_sample(models(en(0), form(x), []),20,P).
 % expected result 1
-% The values for s1 can be 0+, and for x from 2 to 7.
+% The values for en can be 0+.
+?- mc_sample(models(en(1), form(x), []),1, P).
+% expected result 1
 */
 :- use_module(library(mcintyre)).
 
@@ -57,38 +60,61 @@ all_models([SW|Rest], S, F, H) :-
 % Negated formulas are omitted
 
 
-temporal(tabled_models(_,_,_)).
+% 1-exit RMC example for the slow-converging x = 1/2 (1 + x^2)
+% equation, scaled to repeat it multiple times.
 
-% This is a simple example with entanglement that can scale the
-% formula and the model.  Here, t is essentially a step down.
-%
-% The formula x can take values from 2 to 7.  When it's at 2:
-%
-% x = (<b>x /\ <c>t) \/ (<b>t /\ <c>x) \/ (<b>t /\ <c>t),
-%
-% t = [a]x \/ <b>[a]x
+fdef(x, or(diam(e, tt),
+           or(diam(p, form(x)),
+              and(diam(c, form(x)), diam(r, form(x)))))).
 
-% trans(s1(N), a, s1(N, a)).
-% trans(s1(N), b, s1(N, b)).
-% trans(s1(N), c, s1(N, c)).
-% trans(s1(N), d, s1(N, d)).
-trans(s1(N), A, s1(N, A)).
-trans(s2(N), a, s2(N, a)) :- N>0.
+trans(en(N), p, en(N, p)).
+trans(ex(N), e, ex(N, e)).
+trans(l1x(N), p, l1x(N, p)).
+trans(l1e(N), c, l1e(N, c)).
+trans(l1e(N), r, l1e(N, r)).
+trans(l2x(N), p, l2x(N, p)).
+trans(l2e(N), c, l2e(N, c)).
+trans(l2e(N), r, l2e(N, r)).
+trans(l3x(N), p, l3x(N, p)).
+trans(l3e(N), c, l3e(N, c)).
+trans(l3e(N), r, l3e(N, r)).
 
-% values(s1(_N, a), [s3]).
-% values(s1(N, b), [s1(N), s2(N)]).
-% values(s1(N, c), [s1(N), s2(N)]).
-values(s1(_N, a), [s3]) :- !.
-values(s1(N, _A), [s1(N), s2(N)]).
-values(s2(N, a), [s1(M)]) :- M is N-1.
+values(en(0, p), [l1e(0), ex(0)]).
+values(en(N, p), [l1e(N), l3e(N)]) :- N>0.
 
-% set_sw(s1(_N, a), [1]).
-% set_sw(s1(_N, b), [0.3, 0.7]).
-% set_sw(s1(_N, c), [0.5, 0.5]).
-set_sw(s1(_N, a), [1]) :- !.
-set_sw(s1(_N, b), [0.2, 0.8]) :- !.
-set_sw(s1(_N, _A), [0.1, 0.9]).
-set_sw(s2(_N, a), [1]).
+values(ex(N, e), [ex(N)]).
+
+values(l1x(N, p), [l2e(N)]).
+values(l1e(N, c), [en(N)]).
+values(l1e(N, r), [l1x(N)]).
+
+values(l2x(0, p), [ex(0)]).
+values(l2x(N, p), [l3e(N)]) :- N>0.
+
+values(l2e(N, c), [en(N)]).
+values(l2e(N, r), [l2x(N)]).
+
+values(l3x(N, p), [ex(N)]).
+
+values(l3e(N, c), [en(M)]) :-
+        N>0,
+        M is N-1.
+
+values(l3e(N, r), [l3x(N)]).
+
+set_sw(en(_N, p), [0.5, 0.5]).
+set_sw(ex(_N, e), [1]).
+set_sw(l1x(_N, p), [1]).
+set_sw(l1e(_N, c), [1]).
+set_sw(l1e(_N, r), [1]).
+set_sw(l2x(_N, p), [1]).
+set_sw(l2e(_N, c), [1]).
+set_sw(l2e(_N, r), [1]).
+set_sw(l3x(_N, p), [1]).
+set_sw(l3e(_N, c), [1]).
+set_sw(l3e(_N, r), [1]).
+
+
 
 msw(SW,H,V):-
   values(SW,L),
@@ -118,42 +144,6 @@ pick_value(SW,H,PH,I,(P0,V0),(P1,V1)):-
 
 prob_fact(_,_,_,P):P.
 
-% Formulae:
-letter(1, b).
-letter(2, c).
-letter(3, d).
-letter(4, e).
-letter(5, f).
-letter(6, g).
-letter(7, h).
-
-ex_conj(1, N, F, diam(b, form(G))) :- !,
-        (N==1
-        -> G=F
-        ;  G=t(F)
-        ).
-ex_conj(N, C, F, and(diam(L, form(G)), Fs)) :-
-        M is N-1,
-        letter(N, L),
-        ex_conj(M, C, F, Fs),
-        (N==C
-        -> G=F
-        ;  G=t(F)
-        ).
-
-ex_disj(N, 0, F, Fs) :- !,
-        ex_conj(N, 0, F, Fs).
-ex_disj(N, C, F, or(G, Fs)) :-
-        M is C-1,
-        ex_conj(N, C, F, G),
-        ex_disj(N, M, F, Fs).
-
-fdef(x(N), F) :-
-        ex_disj(N, N, x(N), F).
-
-fdef(t(F), or(box(a, form(F)), diam(b, form(n(F))))).
-
-fdef(n(F), box(a, form(F))).
 
 :-end_lpad.
 
