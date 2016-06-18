551ab6e3cdc048c4846e4acc19282984f680207f
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-06-18 22:57:00 +0200

    truel comments

--- prolog/examples/duel.pl
+++ prolog/examples/duel.pl
@@ -3,34 +3,12 @@ duello a tre, vedi libro di federico peiretti il grande gioco dei numeri
 */
 
 /** <examples>
-% see http://www.prismmodelchecker.org/casestudies/synchronous_leader.php
-% What is the probability that eventually a leader is elected?
-?- best_strategy([a,b,c],S).
+?- survives([a,b,c],a,0).
+?- mc_sample(survives([a,b,c],a,0),5,P).
+best_strategy(A,Rest,L,T,S,P):-
+?- best_strategy(a,[b,c],[a,b,c],0,S,P).
 % expected result 1
 
-% What is the probability of electing a leader within 3 rounds?
-?- mc_sample(bounded_eventually(elect,3),1000,P).
-% expected result 0.97
-
-% What is the expected number of rounds to elect a leader?
-?- mc_expectation(eventually(elect,T),1000,T,E).
-% expected result 1.2
-
-
-?- graph_prob(G). 
-% graph the probability of electing a leader within L rounds as
-% a function of L
-
-?- graph_exp_rounds_n(G).
-% graph the expected number of rounds to elect a leader as a
-% funtion of the number of processes when K=3
-
-?- graph_exp_rounds_k(G).
-% graph the expected number of rounds to elect a leader as a
-% funtion of K when N=3
-
-?- network_topology(G).
-% draw a graph representing the network topology
 */
 :- use_module(library(mcintyre)).
 
@@ -55,15 +33,15 @@ survives_round([],L,_,_,L).
 
 survives_round([H|Rest],L0,A,T,L):-
 	write(survives_round([H|Rest],L0,A,T,L)),nl,
-    best_strategy(H,Rest,L0,0,S,_P),
-    write(best_strategy(H,Rest,L0,T,S,_P)),nl,
+    table(best_strategy(H,Rest,L0,0,S,P)),
+    write(best_strategy(H,Rest,L0,T,S,P)),nl,
     write(a=A),nl,
     shoot(H,S,Rest,L0,T,Rest1,L1),
     write(shoot(H,S,Rest,L0,T,Rest1,L1)),nl,
     member(A,L1),
     survives_round(Rest1,L1,A,T,L).
 
-survives_bounded(_,_,s(s(0))):-!.
+%survives_bounded(_,_,s(s(s(s(s(s(0))))))):-!.
 
 survives_bounded([A],A,_):-!.
 
@@ -77,7 +55,7 @@ survives_round_bounded([],L,_,_,L).
 
 survives_round_bounded([H|Rest],L0,A,T,L):-
 	write(survives_round([H|Rest],L0,A,T,L)),nl,
-    best_strategy(H,Rest,L0,T,S,_P),
+    table(best_strategy(H,Rest,L0,T,S,_P)),
     write(best_strategy(H,Rest,L0,T,S,_P)),nl,
     write(a=A),nl,
     shoot(H,S,Rest,L0,T,Rest1,L1),
@@ -91,12 +69,23 @@ write(best_strategy(A,Rest,L,T,S,P)),nl,
     delete(L,A,L1),
     write(l1=L1),nl,
     %maplist(ev_action(A,Rest,T,L),[sky|L],LP),
-    maplist(ev_action(A,Rest,T,L),[sky|L1],LP),
+    append(L1,[sky],L2),
+    maplist(ev_action(A,Rest,T,L),L2,LP),
     write(LP),nl,
-    max_member(P-S,LP).
+    keysort(LP,[P-S|_]).
+%    max_member(P-S,LP).
 
 ev_action(A,Rest,T,L,S,P-S):-
-    mc_sample(survives_rest(A,Rest,T,L,S),10,P).
+    mc_sample(survives_rest(A,Rest,T,L,S),5,P).
+
+%ev_action(A,Rest,T,L,S,P-S):-
+%    mc_sample(survives_rest1(A,Rest,T,L,S),5,P).
+
+survives_rest1(A,Rest0,T,L0,S):-
+    shoot(A,S,Rest0,L0,T,Rest,L1),
+    write((survives_rest(A,Rest0,T,L0,S),shoot(A,S,Rest0,L0,T,Rest,L1))),nl,
+    survives_round_bounded(Rest,L1,A,T,L),
+    survives_bounded(L,A,s(T)).
 
 survives_rest(A,Rest0,T,L0,S):-
     shoot(A,S,Rest0,L0,T,Rest,L1),
@@ -119,6 +108,12 @@ shoot(H,S,Rest0,L0,T,Rest,L):-
     ).
 
 
+table(A):-
+  tabled(A),!.
+
+table(A):-
+  call(A),
+  assert(tabled(A)).
 
 hit(_,a):1/3.
 
@@ -130,7 +125,7 @@ hit(_,c):1.
 
 :- end_lpad.
 
-
+:- retractall(tabled(_)).
 
 
 
