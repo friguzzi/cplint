0e0028c09de4060f7e28c70f6407ff7b2ac4c3dc
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2017-07-04 13:02:35 +0200

    more info

--- prolog/examples/lemur/testmach.pl
+++ prolog/examples/lemur/testmach.pl
@@ -3,6 +3,8 @@
 
 
 test((induce_lm([train],P),test_lm(P,[test],LL,AUCROC,_ROC,AUCPR,_PR),
+writeln(P),
+writeln((LL,AUCROC,AUCPR)),
 P = [(class_fix:0.167671594180088;'':0.832328405819912:-not_replaceable(_92), replaceable(_96), replaceable(_100), worn(_96), not_worn(_92)),
   (class_sendback:1.0;'':0.0:-not_replaceable(_112), worn(_112)),  
   (class_ok:1.0;'':0.0:-none_worn),
