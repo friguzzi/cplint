99b2e5bf891e25bd40b85a1ce5a5b717e8dd4de9
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2017-08-11 19:02:07 +0200

    new test data for mps

--- prolog/examples/learning/testmps.pl
+++ prolog/examples/learning/testmps.pl
@@ -1,8 +1,11 @@
 
 
 
-test((induce_par([train],P),writeln(P),
-writeln([(p:1.0:-a, b),  (p:1.0:- \+a, \+b),
-(a:0.16899889957827613;'':0.8310011004217239:-true), 
- (b:0.5023596826993204;'':0.4976403173006796:-true)])
+test((induce_par([train],P), test_prob(P,[train],_NP,_NN,LL,_EL),
+writeln(P),
+writeln(
+[(p:1.0:-a, b),  (p:1.0:- \+a, \+b),  (a:0.14098536470750564;'':0.8590146352924943:-true),
+(b:0.2686080357352204;'':0.7313919642647796:-true)]),
+writeln([0.6661462033202675-p(train1), 0.6661462033202675-(\+p(train2)),
+0.6661462033202675-p(train3)]),close_to(LL, -1.9095443323886057)
 ),multiple_paths_simple_learning).
