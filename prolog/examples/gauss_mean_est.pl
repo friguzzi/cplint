deb470de9d6e788442ba0a8ba5a7351558657a72
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2017-10-20 20:37:12 +0200

    particle filtering for gauss mean est

--- prolog/examples/gauss_mean_est.pl
+++ prolog/examples/gauss_mean_est.pl
@@ -1,12 +1,12 @@
 /*
 Posterior estimation in Bayesian models.
 We are trying to estimate the true value of a Gaussian distributed random
-variable, given some observed data. The variance is known (2) and we 
+variable, given some observed data. The variance is known (2) and we
 suppose that the mean has a Gaussian distribution with mean 1 and variance
 5. We take different measurement (e.g. at different times), indexed
 with an integer.
-Given that we observe 9 and 8 at indexes 1 and 2, how does the distribution 
-of the random variable (value at index 0) changes with respect to the case of 
+Given that we observe 9 and 8 at indexes 1 and 2, how does the distribution
+of the random variable (value at index 0) changes with respect to the case of
 no observations?
 From
 http://www.robots.ox.ac.uk/~fwood/anglican/examples/viewer/?worksheet=gaussian-posteriors
@@ -19,7 +19,7 @@ http://www.robots.ox.ac.uk/~fwood/anglican/examples/viewer/?worksheet=gaussian-p
 :- mc.
 :- begin_lpad.
 
-value(I,X) :- 
+value(I,X) :-
   mean(M),
   value(I,M,X).
 % at time I we see X sampled from a Gaussian with mean M and variamce 2.0
@@ -36,7 +36,7 @@ value(_,M,X): gaussian(X,M, 2.0).
 hist_uncond(Samples,NBins,Chart):-
   mc_sample_arg(value(0,X),Samples,X,L0),
   histogram(L0,NBins,Chart).
-% plot an histogram of the density of the random variable before any 
+% plot an histogram of the density of the random variable before any
 % observations by taking Samples samples and by dividing the domain
 % in NBins bins
 
@@ -44,16 +44,27 @@ dens_lw(Samples,NBins,Chart):-
   mc_sample_arg(value(0,Y),Samples,Y,L0),
   mc_lw_sample_arg(value(0,X),(value(1,9),value(2,8)),Samples,X,L),
   densities(L0,L,NBins,Chart).
-% plot the densities of the random variable before and after  
-% observing 9 and 8 by taking Samples samples and by dividing the domain
-% % in NBins bins
+% plot the densities of the random variable before and after
+% observing 9 and 8 by taking Samples samples using likelihood weighting
+% and by dividing the domain
+% in NBins bins
 
-
+dens_part(Samples,NBins,Chart):-
+  mc_sample_arg(value(0,Y),Samples,Y,L0),
+  mc_particle_sample_arg(value(0,X),[value(1,9),value(2,8)],Samples,X,L),
+  densities(L0,L,NBins,Chart).
+% plot the densities of the random variable before and after
+% observing 9 and 8 by taking Samples samples using particle filtering
+% and by dividing the domain
+% in NBins bins
 
 /** <examples>
 ?- dens_lw(1000,40,G).
 % plot the densities of the random variable before and after
-% observing 9 and 8
+% observing 9 and 8 using likelihood weighting
+?- dens_part(1000,40,G).
+% plot the densities of the random variable before and after
+% observing 9 and 8 using particle filtering
 ?- hist_uncond(10000,40,G).
 % plot an histogram of the density of the random variable before any
 % observations
@@ -63,4 +74,3 @@ dens_lw(Samples,NBins,Chart):-
 % E = 0.9698875384639362.
 
 */
- 
