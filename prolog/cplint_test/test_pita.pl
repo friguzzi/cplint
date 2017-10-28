30b083a076116576120f76c90778ed05b1f41c68
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2017-10-28 23:30:44 +0200

    test pitavit pl unit test

--- prolog/cplint_test/test_pita.pl
+++ prolog/cplint_test/test_pita.pl
@@ -353,5 +353,91 @@ test(ev):-
 	  Exp=[
 		  rule(0, red(b1), [red(b1):0.6, green(b1):0.3, blue(b1):0.1], pick(b1))
 		  ])).
-
 :- end_tests(map_es2map1).
+
+:- begin_tests(pitavit_win, []).
+
+:-ensure_loaded(library(examples/pitavit_win)).
+test(win):-
+  run((vit_prob(win,P,Exp),close_to(P,0.36),
+Exp=[
+  rule(0, red, [red:0.4, '':0.6], []),
+  rule(1, green, [green:0.9, '':0.09999999999999998], [])])).
+
+:- end_tests(pitavit_win).
+
+:- begin_tests(pitavit_hmm, []).
+
+:-ensure_loaded(library(examples/pitavit_hmm)).
+
+test(hmm_a_g_g):-
+  run((vit_prob(hmm([a,g,g]),P,Exp),close_to(P,0.000405),
+  Exp= [rule(0,next_state(q1,q2,[]),[next_state(q1,q1,[]):0.5,
+	    next_state(q1,q2,[]):0.45,next_state(q1,end,[]):0.05],[]),
+		rule(2,letter(q1,a,[]),[letter(q1,a,[]):0.4,letter(q1,c,[]):0.3,
+		  letter(q1,g,[]):0.2,letter(q1,t,[]):0.1],[]),
+		rule(1,next_state(q2,q2,[q1]),[next_state(q2,q1,[q1]):0.45,
+		  next_state(q2,q2,[q1]):0.5,next_state(q2,end,[q1]):0.05],[]),
+		rule(3,letter(q2,g,[q1]),[letter(q2,a,[q1]):0.1,letter(q2,c,[q1]):0.2,
+		  letter(q2,g,[q1]):0.3,letter(q2,t,[q1]):0.4],[]),
+		rule(1,next_state(q2,end,[q2,q1]),[next_state(q2,q1,[q2,q1]):0.45,
+		  next_state(q2,q2,[q2,q1]):0.5,next_state(q2,end,[q2,q1]):0.05],[]),
+		rule(3,letter(q2,g,[q2,q1]),[letter(q2,a,[q2,q1]):0.1,
+		  letter(q2,c,[q2,q1]):0.2,letter(q2,g,[q2,q1]):0.3,
+			letter(q2,t,[q2,q1]):0.4],[])]
+  )).
+
+
+
+test(hmm_a_a_a):-
+  run((vit_prob(hmm([a,a,a]),P,Exp),close_to(P,0.0008000000000000003),
+  Exp = [rule(0,next_state(q1,q1,[]),[next_state(q1,q1,[]):0.5,
+	  next_state(q1,q2,[]):0.45,next_state(q1,end,[]):0.05],[]),
+	rule(2,letter(q1,a,[]),[letter(q1,a,[]):0.4,letter(q1,c,[]):0.3,
+	  letter(q1,g,[]):0.2,letter(q1,t,[]):0.1],[]),
+	rule(0,next_state(q1,q1,[q1]),[next_state(q1,q1,[q1]):0.5,
+	  next_state(q1,q2,[q1]):0.45,next_state(q1,end,[q1]):0.05],[]),
+	rule(2,letter(q1,a,[q1]),[letter(q1,a,[q1]):0.4,letter(q1,c,[q1]):0.3,
+	  letter(q1,g,[q1]):0.2,letter(q1,t,[q1]):0.1],[]),
+	rule(0,next_state(q1,end,[q1,q1]),[next_state(q1,q1,[q1,q1]):0.5,
+	  next_state(q1,q2,[q1,q1]):0.45,next_state(q1,end,[q1,q1]):0.05],[]),
+	rule(2,letter(q1,a,[q1,q1]),[letter(q1,a,[q1,q1]):0.4,
+	  letter(q1,c,[q1,q1]):0.3,letter(q1,g,[q1,q1]):0.2,
+	  letter(q1,t,[q1,q1]):0.1],[])]
+  )).
+
+:- end_tests(pitavit_hmm).
+
+
+:- begin_tests(pitavit_coin, []).
+
+:-ensure_loaded(library(examples/pitavit_coin)).
+
+test(h_c):-
+  run((vit_prob(heads(coin),Prob,Exp),close_to(Prob,0.45),
+  Exp = [rule(2, fair(coin), [fair(coin):0.9, biased(coin):0.1], []),
+    rule(0, heads(coin), [heads(coin):0.5, tails(coin):0.5],
+    [toss(coin), \+biased(coin)])]
+  )).
+
+:- end_tests(pitavit_coin).
+
+:- begin_tests(pitavit_mendel, []).
+
+:-ensure_loaded(library(examples/pitavit_mendel)).
+
+
+test(s_p):-
+  run((vit_prob(color(s,purple),Prob,Exp),close_to(Prob,0.5),
+	Exp = [rule(0, cg(s, 1, p), [cg(s, 1, p):0.5, cg(s, 1, w):0.5],
+	 [mother(m, s), cg(m, 1, p), cg(m, 2, w)])])).
+
+
+test(s_w):-
+  run((vit_prob(color(s,white),Prob,Exp),close_to(Prob,0.25),
+  Exp = [rule(0, cg(s, 1, w), [cg(s, 1, p):0.5, cg(s, 1, w):0.5],
+     [mother(m, s), cg(m, 1, p), cg(m, 2, w)]),
+	 rule(1, cg(s, 2, w), [cg(s, 2, w):0.5, cg(s, 2, p):0.5],
+	   [father(f, s), cg(f, 1, w), cg(f, 2, p)])])).
+
+:- end_tests(pitavit_mendel).
