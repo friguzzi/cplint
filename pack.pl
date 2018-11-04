3fcf1f26d442490a319b3a210ee808cad2684d5d
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2018-11-04 15:22:36 +0100

    bddem in the requirements

--- pack.pl
+++ pack.pl
@@ -3,5 +3,6 @@ title('A suite of programs for reasoning with probabilistic logic programs').
 version('4.1.0').
 author('Fabrizio Riguzzi','fabrizio.riguzzi@unife.it').
 download('https://github.com/friguzzi/cplint/releases/*.zip').
+requires(bddem).
 requires(auc).
 requires(matrix).
