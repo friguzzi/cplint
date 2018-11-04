da8b6a53d31450959d7413d0b5c7add24f145ab8
Fabrizio Riguzzi <fabrizio.riguzz@unife.it>
2018-11-04 10:37:05 +0100

    updated version and binary for windows to reflect the new C interface

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(cplint).
 title('A suite of programs for reasoning with probabilistic logic programs').
-version('4.0.0').
+version('4.1.0').
 author('Fabrizio Riguzzi','fabrizio.riguzzi@unife.it').
 download('https://github.com/friguzzi/cplint/releases/*.zip').
 requires(auc).
