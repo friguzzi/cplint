6652d0989af761b4f3a0bf13fe4b3f05455d51ec
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2022-02-01 17:52:58 +0100

    solved issue #43 Flexible probabilities with ProbLog syntax

--- prolog/pita.pl
+++ prolog/pita.pl
@@ -1159,49 +1159,37 @@ to_table(M,Heads,ProcTabDir,Heads1):-
 
 tab_dir(_M,'':_,[],[]):-!.
 
-tab_dir(M,H:P,[],[H:P]):-
-  M:tabled(H),!.
-
 % tab dir for decision variables
 % merge with the previous one?
 % the predicates are equal except
 % (?)::H and H:P
-tab_dir(M,HT,[],[H]):-
-  (HT = ?::H ; HT = (?)::H),
+tab_dir(M,D::H,[],[H]):-
+  (D == ? ; D == (?)),
   M:tabled(H),!.
 % tab dir for decision variables
 % merge with the previous one?
 % the predicates are equal except
 % (?)::H and '$util'(A,B)
 tab_dir(M,H,[],[H]):-
-  M:tabled(H),!,
   H=..[F|_],
-  F = utility.
+  F = utility,
+  M:tabled(H),!.
+
+tab_dir(M,H:P,[],[H:P]):-
+  M:tabled(H),!.
+
+tab_dir(M,P::H,[],[H:P]):-
+  P \== ?,
+  M:tabled(H),!.
 
 
-tab_dir(M,H:P,[(:- table HT)],[H1:P]):-
-  functor(H,F,A0),
-  functor(PT,F,A0),
-  PT=..[F|Args0],
-  (M:local_pita_setting(depth_bound,true)->
-    ExtraArgs=[_,_,lattice(orc/3)]
-  ;
-    ExtraArgs=[_,lattice(orc/3)]
-  ),
-  append(Args0,ExtraArgs,Args),
-  HT=..[F|Args],
-  H=..[_|ArgsH],
-  H1=..[F|ArgsH],
-  assert(M:tabled(PT)),
-  zero_clause(M,F/A0,LZ),
-  assert(M:zero_clauses(LZ)).
 
 % tab dir for decision variables
 % merge with the previous one?
 % the predicates are equal
 % except variable n 2 and 4.
-tab_dir(M,HT1,[(:- table HT)],[H1]):-
-  (HT1 = ?::H ; HT1 = (?)::H),
+tab_dir(M,P::H,[(:- table HT)],[H1]):-
+  (P== ?;P == (?)),!,
   functor(H,F,A0),
   functor(PT,F,A0),
   PT=..[F|Args0],
@@ -1224,7 +1212,7 @@ tab_dir(M,HT1,[(:- table HT)],[H1]):-
 % except variable n 2 and 4.
 tab_dir(M,H,[(:- table HT)],[H1]):-
   H=..[F|_],
-  F = utility,
+  F = utility,!,
   functor(H,F,A0),
   functor(PT,F,A0),
   PT=..[F|Args0],
@@ -1240,6 +1228,26 @@ tab_dir(M,H,[(:- table HT)],[H1]):-
   assert(M:tabled(PT)),
   zero_clause(M,F/A0,LZ),
   assert(M:zero_clauses(LZ)).
+
+
+tab_dir(M,Head,[(:- table HT)],[H1:P]):-
+  (Head=H:P;Head=P::H),!,
+  functor(H,F,A0),
+  functor(PT,F,A0),
+  PT=..[F|Args0],
+  (M:local_pita_setting(depth_bound,true)->
+    ExtraArgs=[_,_,lattice(orc/3)]
+  ;
+    ExtraArgs=[_,lattice(orc/3)]
+  ),
+  append(Args0,ExtraArgs,Args),
+  HT=..[F|Args],
+  H=..[_|ArgsH],
+  H1=..[F|ArgsH],
+  assert(M:tabled(PT)),
+  zero_clause(M,F/A0,LZ),
+  assert(M:zero_clauses(LZ)).
+
 
 pita_expansion(begin_of_file,_):-
   !,
