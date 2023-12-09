000b02a5d279f8322035d1acb2b4642a884f0ccf
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2023-12-09 09:49:20 +0100

    code for building pldoc

---
+++ docs/gen_pldoc.pl
@@ -0,0 +1,19 @@
+:- use_module(library(pita)).
+:- use_module(library(mcintyre)).
+:- use_module(library(slipcover)).
+:- use_module(library(viterbi)).
+:- use_module(library(cplint_util)).
+:- use_module(library(kbest)).
+:- use_module(library(pitaind)).
+:- use_module(library(lemur)).
+
+:- doc_save('../prolog/pita.pl',[doc_root('./pldoc'),index_file(pita)]).
+:- doc_save('../prolog/mcintyre.pl',[doc_root('./pldoc'),index_file(mcintyre)]).
+:- doc_save('../prolog/slipcover.pl',[doc_root('./pldoc'),index_file(slipcover)]).
+:- doc_save('../prolog/viterbi.pl',[doc_root('./pldoc'),index_file(viterbi)]).
+:- doc_save('../prolog/cplint_util.pl',[doc_root('./pldoc'),index_file(cplint_util)]).
+:- doc_save('../prolog/kbest.pl',[doc_root('./pldoc'),index_file(kbest)]).
+:- doc_save('../prolog/pitaind.pl',[doc_root('./pldoc'),index_file(pitaind)]).
+:- doc_save('../prolog/lemur.pl',[doc_root('./pldoc'),index_file(lemur)]).
+
+:- halt.
